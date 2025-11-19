9c572ca35b3b9241e1bd5480daec552f4dadfd9b
mak7 <mak77chek@gmail.com>
2025-11-19 16:21:03 +0200

    зміна налаштування Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import htmlInject from 'vite-plugin-html-inject';
 
 export default defineConfig({
     plugins: [
-        htmlInject(),
+        inject(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                contacts: resolve(__dirname, 'contacts.html'),
+                doctors: resolve(__dirname, 'doctors.html'),
+                services: resolve(__dirname, 'services.html'),
+            },
+        },
+    },
 });
